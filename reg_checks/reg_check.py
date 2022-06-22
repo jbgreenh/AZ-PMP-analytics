94f536eb49ea8381c7660c3a4505998dcfec1c7c
jbgreenh <jbgreenh@asu.edu>
2022-06-22 15:21:40 -0700

    added utils

--- reg_checks/reg_check.py
+++ reg_checks/reg_check.py
@@ -1,15 +1,5 @@
 import pandas as pd
-
-def highlight_cells(val):
-    match val:
-        case 'NO':
-            color = '#ffcccb'   # light red
-        case 'YES':
-            color = '#d2f8d2'   # light green
-        case _:
-            color = 'transparent'
-
-    return 'background-color: {}'.format(color)
+from ..utils.sheet_formatting import *
 
 def check_reg(input_fn, output_fn, dea_col_name):
     awarxe = pd.read_excel('data/awarxe.xlsx', skiprows=1, index_col=None)
@@ -22,11 +12,7 @@ def check_reg(input_fn, output_fn, dea_col_name):
 
     output.style.applymap(highlight_cells).to_excel(writer, index=False, sheet_name='registration', engine='xlsxwriter')
 
-    # set column width based on max length in column
-    for col in output:
-        col_len = max(output[col].astype(str).map(len).max(), len(col)) + 2
-        col_i = output.columns.get_loc(col)
-        writer.sheets['registration'].set_column(col_i, col_i, col_len)
+    set_col_widths(writer, output, 'registration')
 
     writer.save()
     print(f'{output_fn} saved')
---
+++ utils/sheet_formatting.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+def highlight_cells(val: str):
+    '''highlight cells based on YES or NO'''
+    match val:
+        case 'NO':
+            color = '#ffcccb'   # light red
+        case 'YES':
+            color = '#d2f8d2'   # light green
+        case _:
+            color = 'transparent'
+
+    return 'background-color: {}'.format(color)
+
+def set_col_widths(writer: pd.ExcelWriter, df: pd.DataFrame, sheet_name: str):
+    '''set column width based on max length in column'''    
+    for col in df:
+        col_len = max(df[col].astype(str).map(len).max(), len(col)) + 2
+        col_i = df.columns.get_loc(col)
+        writer.sheets[sheet_name].set_column(col_i, col_i, col_len)
