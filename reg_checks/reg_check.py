78123803e6fa84abb3361ad36b99b638b19633bb
jbgreenh <jbgreenh@asu.edu>
2022-06-23 16:19:45 -0700

    file name -> file path

--- reg_checks/reg_check.py
+++ reg_checks/reg_check.py
@@ -1,27 +1,27 @@
 import pandas as pd
 from utils.sheet_formatting import *
 
-def check_reg(input_fn, output_fn, dea_col_name):
+def check_reg(input_fp, output_fp, dea_col_name):
     awarxe = pd.read_excel('data/awarxe.xlsx', skiprows=1, index_col=None)
-    input = pd.read_excel(input_fn, index_col=None)
+    input = pd.read_excel(input_fp, index_col=None)
 
     output = input.assign(awarxe=input[dea_col_name].isin(awarxe['DEA Number']))
     output['awarxe'] = output['awarxe'].map({True:'YES' ,False:'NO'})
 
-    writer = pd.ExcelWriter(output_fn)
+    writer = pd.ExcelWriter(output_fp)
 
     output.style.applymap(highlight_cells).to_excel(writer, index=False, sheet_name='registration', engine='xlsxwriter')
 
     set_col_widths(writer, output, 'registration')
 
     writer.save()
-    print(f'{output_fn} saved')
+    print(f'{output_fp} saved')
 
 def main():
-    in_fn = input('input file name? ex: data/test.xlsx\n')
-    out_fn = input('output file name? ex: ~/Downloads/avpq.xlsx\n')
-    dea_col = input('dea number column name? ex: DEA Number\n')
-    check_reg(input_fn=in_fn, output_fn=out_fn, dea_col_name=dea_col)
+    in_fp = input('input file path? ex: data/test.xlsx\n')
+    out_fp = input('output file path? ex: ~/Downloads/avpq.xlsx\n')
+    dea_col = input('dea number column path? ex: DEA Number\n')
+    check_reg(input_fp=in_fp, output_fp=out_fp, dea_col_name=dea_col)
 
 if __name__ == "__main__":
     main()
