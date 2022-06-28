f39356ab32041386b99fab7aea5619a989bdcfdb
jbgreenh <jbgreenh@asu.edu>
2022-06-28 10:58:34 -0700

    added az_pharmacy_deas.py

---
+++ deas/az_pharmacy_deas.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+# widths and names according to the file format specifications provided by the DEA
+dea_widths = [9, 1, 2, 40, 40, 40, 40, 33, 2, 9, 8, 8, 12, 3, 9, 13, 15, 15]
+dea_names = [
+    'DEA Number', 'Business Activity Code', 'Business Activity Sub Code', 'Name', 
+    'Address 1', 'Address 2', 'Address 3', 'City', 'State', 'Zip Code', 'Date of Original Registration',
+    'Expiration Date', 'Drug Schedules', 'Degree', 'SSN', 'Tax ID', 'State License Number', 'State CS License Number'
+    ]
+
+print('trying to read deas')
+deas = pd.read_fwf('data/cs_active.txt', widths=dea_widths, names=dea_names, encoding='latin-1')
+deas = deas[(deas['Business Activity Code'] == 'A')]
+deas = deas[deas['State'] == 'AZ']
+deas.to_csv('data/az_pharmacy_deas.csv', index=False)
+print('updated data/az_pharmacy_deas.csv')
