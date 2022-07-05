62d2cffd4dfb2f34872b5121fa6c4a7520edd7c8
jbgreenh <jbgreenh@asu.edu>
2022-07-05 14:53:44 -0700

    fixed some whitespace

--- pharmacy_cleanup/pharm_clean.py
+++ pharmacy_cleanup/pharm_clean.py
@@ -5,8 +5,8 @@ igov = pd.read_csv('data/igov_pharmacy.csv', index_col=None)
 mp = pd.read_csv('data/manage_pharmacies.csv', index_col=None)
 
 ddr = ddr[ddr['Days Delinquent'] >= 20]
-ddr = pd.merge(ddr,mp[['DEA', 'Pharmacy License Number']], on='DEA', how='left')
-ddr = pd.merge(ddr,igov[['License/Permit #', 'Status']], left_on='Pharmacy License Number', right_on='License/Permit #', how='left').drop(['License/Permit #'], axis=1)
+ddr = pd.merge(ddr, mp[['DEA', 'Pharmacy License Number']], on='DEA', how='left')
+ddr = pd.merge(ddr, igov[['License/Permit #', 'Status']], left_on='Pharmacy License Number', right_on='License/Permit #', how='left').drop(['License/Permit #'], axis=1)
 ddr.sort_values(['Status', 'Pharmacy License Number'], ascending=False, inplace=True)
 
 ddr.to_clipboard(index=False)
