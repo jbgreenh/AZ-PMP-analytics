ae800e0fc9c2d10689035b8aa296c72168cad1ec
jbgreenh <jbgreenh@asu.edu>
2022-07-07 16:00:48 -0700

    clean awarxe and generate awarxe_code for matching

--- medical_marijuana/mm.py
+++ medical_marijuana/mm.py
@@ -1,9 +1,23 @@
 import pandas as pd
 
-deg_for_trimming = [' DO', ' MD']   # add degrees to be trimmed as needed
-# for removing the end of a string if it matches trailing
 def trim_string(astring, trailing):
+    '''trim trailing characters from a string'''
     thelen = len(trailing)
     if astring[-thelen:] == trailing:
         return astring[:-thelen]
     return astring
+
+awarxe = pd.read_excel('data/awarxe.xlsx', skiprows=1, index_col=None)
+awarxe = awarxe[['Last Name', 'Professional License Number', 'DEA Number']]
+awarxe = awarxe[~awarxe['DEA Number'].isnull()]
+awarxe['Last Name'] = awarxe['Last Name'].str.upper()
+
+deg_for_trimming = [', DO', ', MD', ', PA', ', NP']   # add degrees to be trimmed as needed
+
+for deg in deg_for_trimming:
+    awarxe['Last Name'] = awarxe['Last Name'].apply(lambda x: trim_string(x, deg))
+
+awarxe['awarxe_code'] = awarxe['Last Name'].str[-3:] + awarxe['Professional License Number'].str[-4:]
+
+awarxe.to_csv('~/Downloads/test_awarxe.csv', index=False)
+
