6df2d0ae38c97754a0fadb5c94be6cae6fe1364f
Jake Greenhalgh <48000025+jbgreenh@users.noreply.github.com>
2022-09-08 13:37:37 -0700

    added a column width formatter for openpyxl

--- utils/sheet_formatting.py
+++ utils/sheet_formatting.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from openpyxl.styles import PatternFill
 
 def highlight_cells(val: str):
     '''highlight cells based on YES or NO'''
@@ -18,3 +19,11 @@ def set_col_widths(writer: pd.ExcelWriter, df: pd.DataFrame, sheet_name: str):
         col_len = max(df[col].astype(str).map(len).max(), len(col)) + 2
         col_i = df.columns.get_loc(col)
         writer.sheets[sheet_name].set_column(col_i, col_i, col_len)
+
+def set_col_widths_openpyxl(writer: pd.ExcelWriter, df: pd.DataFrame, sheet_name: str):
+    '''set column width based on max length in column'''
+    for col in df:
+        col_len = max(df[col].astype(str).map(len).max(), len(col)) + 2
+        col_i = df.columns.get_loc(col)
+        col_i = chr(ord('A') + col_i)
+        writer.sheets[sheet_name].column_dimensions[col_i].width = col_len
