40983b84c001fedcb74bf2eecadf45a4c89a7c51
Jake Greenhalgh <48000025+jbgreenh@users.noreply.github.com>
2022-09-08 16:39:27 -0700

    reformatted to fit user needs better

--- pharmacy_cleanup/pharm_clean.py
+++ pharmacy_cleanup/pharm_clean.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from datetime import date
 
 ddr = pd.read_csv('data/delinquent_dispenser_request.csv', index_col=None)
 igov = pd.read_csv('data/igov_pharmacy.csv', index_col=None)
@@ -9,6 +10,14 @@ ddr = pd.merge(ddr, mp[['DEA', 'Pharmacy License Number']], on='DEA', how='left'
 ddr = pd.merge(ddr, igov[['License/Permit #', 'Status']], 
     left_on='Pharmacy License Number', right_on='License/Permit #', how='left').drop(columns=['License/Permit #'])
 ddr.sort_values(['Status', 'Pharmacy License Number'], ascending=False, inplace=True)
+
+#get today's date as a string
+today = date.today().strftime("%m/%d/%Y")
+ddr['Date List Pulled'] = today
+
+#rearrange columns
+ddr = ddr[['Pharmacy Name', 'Pharmacy Address', 'Pharmacy License Number', 'DEA', 'Status', 'Days Delinquent', 
+    'Last Compliant', 'Date List Pulled', 'Primary Email', 'Primary Phone']]
 
 ddr.to_clipboard(index=False)
 print('copied to clipboard')
