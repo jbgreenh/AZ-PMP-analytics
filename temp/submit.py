69c11fb45c77847a7aa74c85f410e5562d2f388f
Jake Greenhalgh <48000025+jbgreenh@users.noreply.github.com>
2022-09-22 14:48:00 -0700

    more updates

--- temp/submit.py
+++ temp/submit.py
@@ -1,10 +1,11 @@
 import pandas as pd
 
-submits = pd.read_csv('data/submits.csv', sep='\t', index_col=None)
-manage_pharmacies = pd.read_csv('data/manage_pharmacies.csv', index_col=None)
+submits = pd.read_csv('data/submits.csv', index_col=None)
 
-not_mp = submits[~submits['Dea Number'].isin(manage_pharmacies['DEA'])]
-not_mp = not_mp.sort_values(by=['Dispensary Name', 'License Number'])
-not_mp = not_mp.drop_duplicates(subset=['Dea Number'])
+submits = submits[['Dea Number', 'Measure Values']]
+submits = submits.groupby('Dea Number').sum()
+submits = submits[~submits.index.str.isnumeric()]
+submits.index = submits.index.str.upper()
 
-not_mp.to_clipboard(index=False)
+submits.to_clipboard()
+print('copied to clipboard')
